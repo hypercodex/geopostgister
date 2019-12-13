6e0ea396b8c36b632a7b2d5fea8c8fe56c32a957
wg <wxkxg@protonmail.com>
2019-12-13 13:29:21 -0800

    update user env var

--- src/scripts/util/db.sh
+++ src/scripts/util/db.sh
@@ -6,12 +6,12 @@ source .env
 
 function run_sql {
   # args $1: sql statement
-  psql -d geocoder -U ${PG_GEOCODER_USER} -p 5433 -h localhost -c "$1" 
+  psql -d geocoder -U ${POSTGRES_GIS_USER} -p 5433 -h localhost -c "$1" 
 }
 
 function run_sql_to_file {
   # args $1: sql statement
   #      $2: outfile
-  psql -d geocoder -U ${PG_GEOCODER_USER} -p 5433 -h localhost -c "$1" -tA > $2
+  psql -d geocoder -U ${POSTGRES_GIS_USER} -p 5433 -h localhost -c "$1" -tA > $2
 }
 
